5ee5c8956567437f3a3e58554717a7529306c60c
Anye Happiness Ade <anye.happiness@swisslinkedu.com>
2025-10-18 06:27:46 +0000

    removed files revert version working

--- scripts/test-backup.js
+++
@@ -1,8 +0,0 @@
-// Simple script to test backup functionality
-const { testBackupExport } = require('../lib/test-backup');
-
-async function runTest() {
-  await testBackupExport();
-}
-
-runTest().catch(console.error);
